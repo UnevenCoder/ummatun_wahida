ce98ee47429c5a8b5d11ae0ca45329cd7bbab100
Ameen Shafeeq <ameenshafeeq2005@gmail.com>
2022-08-04 22:26:35 +0300

    yay

--- src/App.svelte
+++ src/App.svelte
@@ -5,48 +5,69 @@
   <section id="banner" class="sec">
     <nav>
       <ol>
-        <li id="selected">Our Story</li>
-        <li>What we do</li>
-        <li>Projects</li>
-        <li>Resources</li>
+        <li class="navlist" id="selected">Our Story</li>
+        <li class="navlist">What we do</li>
+        <li class="navlist">Projects</li>
+        <li class="navlist">Resources</li>
       </ol>
     </nav>
   </section>
   <section id="story" class="sec">
-    <div class="up">
-      <section class="img"><img src="/story.png" alt="" /></section>
-      <section class="text">Introductory paragraph goes here</section>
-    </div>
-    <div class="down">
-      <section class="text">Introductory paragraph goes here</section>
-      <section class="img">
-        <img src="/home page design 2.png" alt="" />
+    <div id="story-box">
+      <section id="storybox1" class="storybox">.</section>
+      <section id="storybox2" class="storybox">
+        <h2>Our Story</h2>
+        <p>
+          We are a group of Muslims from across the world who have assembled to
+          benefit the Ummah in any way we can. We often forget the importance of
+          unity; we are one Ummah, and it is crucial that we work together to
+          benefit our people. Knowledge is given a lot of value in Islam, and
+          that is our main focus in this organization. To spread sahih knowledge
+          of Islam. Alhamdulilah we have an abundance of information out there
+          to learn from, and because of the vast sea of knowledge one can easily
+          become overwhelmed. Our goal is to make it easier to access certain
+          type of knowledge bi ithnillah.
+          <br /> <br />
+          Our group met on a social media platform called Discord. Discord was built
+          for gamers, however, the Muslim community found a way to take advantage
+          of this platform to benefit Muslims from around the world. Muslim discorders
+          have built an online community where we learn beneficial knowledge together
+          and build bonds of brotherhood and sisterhood.
+          <br />
+          <br />
+          We have transformed what was initially made to be a gaming platform, into
+          a place of benefit and growth
+        </p>
       </section>
     </div>
   </section>
   <section id="plans" class="sec">
-    <table>
-      <tr>
-        <td id="h" />
-        <td id="p">
-          <p>
-            paragraph paragraph paragraph paragraph paragraph paragraph
-            paragraphparagraph paragraph paragraph paragraph paragraph paragraph
-            paragraph paragraph paragraph paragraph paragraph paragraph ​
-          </p>
-          <p>
-            his is a great space to write a long text about your company and
-            your services. You can use this space to go into a little more
-            detail about your company. Talk about your team and what services
-            you provide. Tell your visitors the story of how you came up with
-            the idea for your business and what makes you different from your
-            competitors. Make your company stand out and show your visitors who
-            you are.
-          </p>
-          <button>Learn more</button>
-        </td>
-      </tr>
-    </table>
+    <div id="do1">
+      <h1>What we do?</h1>
+    </div>
+    <div id="do2">
+      <section class="do3" id="box1">
+        <div class="tpbox" id="tbb1">.</div>
+        <div class="btbox">
+          <h2>Projects</h2>
+          <h5>Regarding different subjects in Islam</h5>
+        </div>
+      </section>
+      <section class="do2" id="box2">
+        Together we try to find gaps in the Islamic resources that are
+        available. Once those gaps are discovered we hope to fill them with
+        authentic knowledge. All the members have given up their free time to
+        help with this project, May Allah make this a means of sadaqah jarriyah
+        for us all.
+      </section>
+      <section class="do3" id="box3">
+        <div class="tpbox" id="tbb2">.</div>
+        <div class="btbox">
+          <h2>Projects</h2>
+          <h5>Regarding different subjects in Islam</h5>
+        </div>
+      </section>
+    </div>
   </section>
   <section id="team" class="sec">
     <div class="pride"><h1>We Take Pride in Our Numbers</h1></div>
@@ -79,6 +100,7 @@
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200&family=Raleway:wght@600&display=swap");
   @import url("https://fonts.googleapis.com/css2?family=Courier+Prime:wght@500&family=Poppins:wght@200&family=Raleway:wght@600&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Courier+Prime:wght@700&family=Poppins:wght@200&family=Raleway:wght@300&display=swap");
   main {
     text-align: center;
     max-width: 240px;
@@ -98,11 +120,24 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+    background-image: url("/background.jpg");
+    background-size: cover;
   }
   .sec h2 {
     color: black;
   }
 
+  #story-box {
+    width: 90%;
+    height: 90%;
+    display: flex;
+    flex-direction: row;
+    background-color: #8a8a8a;
+  }
+  .storybox {
+    width: 50%;
+    height: 100%;
+  }
   #banner {
     height: 100vh;
     width: 100vw;
@@ -120,13 +155,6 @@
     width: fit-content;
   }
 
-  ol li {
-    padding-left: 30px;
-    color: rgb(255, 255, 255);
-    font-size: 1.45em;
-    padding-top: 25px;
-    font-family: "Courier Prime", monospace;
-  }
   nav {
     padding-top: 30px;
     justify-self: center;
@@ -138,53 +166,42 @@
     height: 70px;
     top: 0;
   }
-
-  .up,
-  .down {
-    min-height: 200px;
-    max-height: 600px;
-    width: 80vw;
-    height: 10vh;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-  }
-  .down {
-    margin-top: 130px;
-  }
-
-  .text {
-    margin-left: -30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    width: 100%;
-    font-size: 18px;
-    font-family: Helvetica, sans-serif;
-    color: #8a8a8a;
-  }
-  .img {
-    display: flex;
-    height: 100%;
-    width: 100%;
-    justify-content: center;
-    align-items: center;
-  }
-  .img img {
-    width: 600px;
-    height: auto;
-  }
-
-  .down img {
-    height: 350px;
-    width: auto;
-  }
   #story {
     min-height: 800px;
     max-height: 1000px;
     display: flex;
     flex-direction: column;
+  }
+
+  #storybox2 {
+    background-color: rgb(24, 11, 61);
+    color: white;
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    text-align: justify;
+  }
+
+  #storybox2 h2 {
+    font-size: 40px;
+    font-family: "Courier New", Courier, monospace;
+    color: whitesmoke;
+    padding-top: 20px;
+  }
+  #storybox2 p {
+    width: 80%;
+    line-height: 30px;
+    font-size: 18px;
+    font-weight: 500;
+    font-family: raleway, sans-serif;
+    margin-top: -10px;
+  }
+  #storybox1 {
+    background-image: url("/imgleft2.jpg");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
   }
 
   .pride h1 {
@@ -231,61 +248,59 @@
 
   #plans {
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     height: 110vh;
-    background-image: url("/banner2.jpeg");
+    background-image: url("/background.jpg");
   }
-
-  table {
-    width: 95vw;
-    max-height: 15000px;
-    min-height: 500px;
-    border: 0.5px solid grey;
+  #do1 {
+    height: 25%;
+    width: 100%;
+    color: azure;
+    font-size: 40px;
+    font-family: "Courier New", Courier, monospace;
   }
-  td {
-    max-height: 1500px;
-    height: 90vh;
-    min-height: 500px;
-  }
-  #h {
-    width: 50%;
-    background-image: url("/imgleft2.jpg");
-    color: white;
-    font-family: poppins-extralight, poppins, sans-serif;
-    font-size: large;
-    text-align: center;
-    -moz-background-size: 100% 100%;
-    -webkit-background-size: 100% 100%;
-    background-size: 100% 100%;
-    background-repeat: no-repeat;
-  }
-
-  #p {
+  #do2 {
+    height: 75%;
     width: 100%;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-family: "open sans", sans-serif;
+    justify-content: space-evenly;
   }
-
-  #p p {
-    width: 40%;
-    font-size: 18px;
+  .do3 {
+    width: 35%;
+    height: 100%;
+  }
+  .do2 {
+    width: 20%;
+    height: 100%;
+    color: white;
     text-align: justify;
-    color: #8a8a8a;
-    line-height: 1.5;
   }
 
-  #p button {
-    background-color: rgb(253, 98, 98);
-    border: none;
-    color: #141416;
-    align-self: center;
-    height: 50px;
-    width: 140px;
-    border-radius: 5px;
+  .tpbox {
+    height: 70%;
   }
-
+  .btbox {
+    margin-top: 0;
+    padding-top: 0;
+    background-color: rgb(24, 11, 61);
+    height: 30%;
+    color: white;
+  }
+  #tbb1 {
+    background-color: black;
+    background-image: url("/2.svg");
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
   /* para font normal normal normal 16px/1.4em 'open sans',sans-serif*/
+
+  .navlist {
+    padding-left: 30px;
+    color: rgb(255, 255, 255);
+    font-size: 1.15em;
+    padding-top: 25px;
+    font-family: "Courier Prime", monospace;
+    text-decoration: underline;
+    font-weight: bolder;
+  }
 </style>
